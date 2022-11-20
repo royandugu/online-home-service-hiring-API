93f43cdf63fa6ccca0f5121e4337370c74d594a2
myGithub123# <royandugu99009@gmail.com>
2022-11-20 17:14:31 +0545

    Initial user schema

--- Models/user.js
+++ Models/user.js
@@ -1,10 +1,29 @@
 const Mongoose=require("mongoose");
 const Schema=new Mongoose.Schema({
+    firstName:{
+        type:String,
+        required:[true, "Please provide your first name"],
+        match:[/^[A-Z][a-z]{2,}$/,"Please provide a valaddid first name"]
+    },
+    lastName:{
+        type:String,
+        required:[true, "Please provide your last name"],
+        match:[/^[A-Z][a-z]{2,}$/,"Please provide a valid last name"]
+    },
+    email:{
+        type:String,
+        required:[true, "Please provide an email address"],
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email',],
+        unique: true,
+    },
+    password:{
+        type:String,
+        match:[/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,"Please provide a valid password"]
+    },
     phoneNumber:{
         type:Number,
         required:[true, "Please provide your phone number"],
-        minlength:10,
-        maxlength:10
+        match:[/^[0-9]{10}$/]
     }
 })
 
