13952d3e40210b27e996dd613187d448838fa075
royandugu <royandugu99009@gmail.com>
2022-12-08 19:44:24 +0545

    Final controller setup

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -11,44 +11,71 @@ const UserModel=require("../Models/user");
 
 const queryObject={};
 
-const sendOtp=async (req,res)=>{
+//Sync 
+const otpIsValid=(userEnteredOtp,actualOtp)=>{
+    if(userEnteredOtp===actualOtp) return true;
+    else return false;
+}
+
+//That goes in route
+const sendPhoneOtp=async (req,res)=>{
     const {phoneNumber} = req.body;
     if(!phoneNumber || phoneNumber.length!=10) throw new BadRequestError("The phone number does not match with the expected format");
-    const currentOtp=otpGenerator.generate(6);
+    const phoneOtp=otpGenerator.generate(6);
     
     //send the OTP to user's device await
     queryObject.phoneNumber=phoneNumber;
-    queryObject.currentOtp=currentOtp;
+    queryObject.phoneOtp=phoneOtp;
     //response
     res.status(StatusCodes.OK).json({message:"OTP sent successfully"});
 }
-const validateOtp=async (req,res)=>{
-    //As a middle ware
+
+//Two validators
+const validatePhoneOtp=async (req,res)=>{
     const {userOtp}=req.body;
-    if(userOtp!==queryObject.currentOtp) throw new AuthenticationError("The provided OTP doesnot match with the one given to you");
-    //response
-    res.status(StatusCodes.OK).json({message:"OTP is valid"});
+    if(otpIsValid(userOtp,queryObject.phoneOtp)) return res.status(StatusCodes.OK).json({message:"Phone OTP is valid"});
+    else {
+        delete queryObject.phoneNumber;
+        delete queryObject.phoneOtp;
+        throw new AuthenticationError("The provided phone OTP doesnot match with the one given to you");
+    }
+}
+const validateEmailOtp=async (req,res)=>{
+    const {userOtp}=req.body;
+    if(otpIsValid(userOtp,queryObject.emailOtp)) {
+        UserModel.create({...queryObject});
+        return res.status(StatusCodes.OK).json({message:"Email OTP is valid"})
+    }
+    else {
+        delete queryObject.email;
+        delete queryObject.emailOtp;
+        throw new AuthenticationError("The provided email OTP doesnot match with the one given to you");
+    }
 }
 
-const registerWithoutEmail=async (req,res)=>{
+//Register
+const register=async (req,res)=>{
     //or user can just directly login using gmail API
     const {firstName,lastName,email}=req.body;
+    
+    queryObject.firstName=firstName;
+    queryObject.lastName=lastName;
+    
     if(!email) {
-        UserModel.create(...req.body);
-        const accessToken=jwt.sign({firstName:firstName},);
-        res.status(StatusCodes.OK).json({message:`user ${firstName} created`, accessToken:""})
+        UserModel.create({...queryObject});
+        //Access token generation
+        res.status(StatusCodes.OK).json({message:`user ${firstName} ${lastName} created`, accessToken:""})
     }
     else{
         const emailOtp=otpGenerator.generate(6);
-        //Send OTP to user's email
+        queryObject.emailOtp=emailOtp;
+        //Mail the otp to the user
     }
     res.status(StatusCodes.OK).json({message:"Email OTP has been sent"});
 }
 
-const registerWithEmail= async (req,res)=>{
-
-}
+//Login
 const login=async (req,res)=>{
 
 }
-module.exports={sendOtp,validateOtp,register,login};
+module.exports={sendPhoneOtp,validatePhoneOtp,validateEmailOtp,register,login};
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ## Note <Registration>
 1. Firstly the phone number is validated through OTP
 2. Then the name and email is entered by the user along with the password
+
+## System flow (User opens the application for the first time) 
+
+Step-1 <br>
+
+User opens the application, there a phone number entering input box is present.
+They enter their number and press the submit button. Here the API calls the route
+where sendPhoneOtp is present. There the phone otp is sent to the user through SMS.
+Now an OTP entering interface is displayed to the user. They enter the OTP and as they
+press the submit button, the application makes call to another route in the API where validatePhoneOtp 
+is present. If the OTP is invalid then AuthenticationError is thrown by the API. Now if it is valid
+then user is moved to the register section. Here user enters their firstName, lastName, password and email(optional). If user doesnot enter their email then their account is created. Somehow if user enters the 
+email and it matches the format, then API emails them another OTP and user is redirected to OTP entering page. Here the user has to enter the OTP mailed to their entered email. If it is correct then their account is created.  
