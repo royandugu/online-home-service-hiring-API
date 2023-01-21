8f42899551a49dd4927de58b2f2a95d9da0da401
royandugu <royandugu99009@gmail.com>
2023-01-21 15:21:03 +0545

    OTP sucesfully sent

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const otpGenerator=require("otp-generator");
 const jwt=require("jsonwebtoken");
 const {StatusCodes}=require("http-status-codes");
-const https=require("https");
+const http=require("https");
 
 //User defined
 const BadRequestError = require("../Error_Handlers/badRequestError");
@@ -13,22 +13,21 @@ const OtpModel=require("../Models/phoneOtp");
 
 //That goes in route
 const sendPhoneOtp=async (req,res)=>{
-    const {phoneNumber} = req.body;//inputted data invalid, check SOCIAIR API to send phoneNumber as a string or a number
+    const {phoneNumber} = req.body;
     const phoneOtp=otpGenerator.generate(6);
     
-    //initial request setup
+    //Token
     const apiToken="Bearer "+process.env.API_TOKEN;
-    const endPointUrl=process.env.SOCI_URL;
-    
     //body setup
     const data =JSON.stringify({
         "message": `${phoneOtp} is your Gharmai-register verification code`,
         "mobile": phoneNumber
     })
 
+    //options setup
     const options = {
-        hostname: 'localhost',
-        path: endPointUrl,
+        hostname: 'sms.sociair.com',
+        path: '/api/sms',
         method: 'POST',
         headers: {
             "Authorization":apiToken,
@@ -37,42 +36,22 @@ const sendPhoneOtp=async (req,res)=>{
         }
     }
 
-    const reqw = https.request(options, resw => {
-        let data = '';
-        console.log('Status Code:', resw.statusCode);
-        resw.on('data', chunk => {
-            data += chunk
+    //Senting the request to sociair API
+    const reqw=http.request(options,(resw)=>{
+        let data="";
+        resw.on('data',(chunk)=>data+=chunk);
+        resw.on('end',()=>{
+                res.status(StatusCodes.OK).json({message:JSON.parse(data)})
+            }
+        )
+        resw.on('error',(error)=>{
+            throw new BadRequestError(error.message);
         })
-        resw.on('end', () => {
-            console.log('Body: ', JSON.parse(data));
-            res.status(resw.statusCode).json({body:JSON.parse(data)});
-        })
-    }).on('error', err => {
-    console.log('Error: ', err.message)
-  })
-
-    reqw.write(data)
-    reqw.end()
-
-    // request({
-    //     url: endPointUrl,
-    //     method: "POST",
-    //     headers:{
-    //         "Authorization":apiToken,
-    //         "Content-Type":"application/json",
-    //         "Accept":"application/json"
-    //     },
-    //     json: true,   // <--Very important!!!
-    //     body: data
-    // }, async (error, response, body)=>{
-    //     res.status(StatusCodes.OK).json({response:response,body:body});
-    // });
-
-    //Axios request
-    //  axios.post(endPointUrl, data, config).then(res=> {
-    //      console.log(res);
-    //      res.status(StatusCodes.OK).json({message:"OTP sent successfully",phoneNumber:phoneNumber});
-    //  }).catch(err=>res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:err.res})) 
+    })
+    
+    //passing our data
+    reqw.write(data);
+    reqw.end();
 }
 
 //Two validators
